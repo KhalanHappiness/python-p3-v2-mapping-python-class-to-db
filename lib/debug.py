9b65b96580849a1926841175a3df1ea8306078fd
happiness <happinesskhalan@gmail.com>
2025-05-22 16:17:21 +0300

    Object relational mapping complete

--- lib/debug.py
+++ lib/debug.py
@@ -6,4 +6,30 @@ from department import Department
 
 import ipdb
 
+Department.drop_table()
+Department.create_table()
+
+payroll = Department.create("Payroll", "Building A, 5th floor")
+print (payroll)
+
+ict = Department.create("Info and Comm Tech", "Times towers" )
+print (ict)
+# payroll.save()
+# print(payroll)
+
+hr = Department.create("Human resource", "Building C, East Wing")
+print(hr)
+
+# hr.save()
+# print(hr)
+
+hr.name = 'HR'
+hr.location = 'Building F, 10th Floor'
+hr.update()
+print (hr)
+
+print("Delete payroll")
+payroll.delete()
+print(payroll)
+
 ipdb.set_trace()
